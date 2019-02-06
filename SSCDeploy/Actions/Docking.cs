7105438e4a44ebb8b8b5dc0a7ba216f3535cb1ea
P4R4DiSI4C <paradisiac.web@hotmail.ch>
2019-02-06 17:08:11 +0100

    Fix

--- SSCDeploy/Actions/Docking.cs
+++ SSCDeploy/Actions/Docking.cs
@@ -99,7 +99,8 @@ namespace SSCDeploy.Actions
                         foreach (KeyValuePair<string, PinnedDir> app in AppsToPin)
                         {
                             sw.WriteLine("Files\\syspin \"" + app.Value.DirToUnpin + "\" c:5387"); //Unpin from taskbar
-                            sw.WriteLine("Files\\syspin \"" + app.Value.DirToUnpin + "\" c:51394"); //Unpin from start
+                            sw.WriteLine("Files\\syspin \"" + app.Value.DirToPin + "\" c:51394"); //Unpin from start
+
                             sw.WriteLine("Files\\syspin \"" + app.Value.DirToPin + "\" c:5386"); //Pin to taskbar
                             sw.WriteLine("Files\\syspin \"" + app.Value.DirToPin + "\" c:51201"); //Pin to start
                         }
